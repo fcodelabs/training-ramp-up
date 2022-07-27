a095c94f3151f5984d526f55d4a0ad9d178ded7a
primesh <primeshheshan@gmail.com>
2022-07-27 15:55:32 +0530

    crud opertaions with dummy data

---
+++ frontend/src/components/sample-students.tsx
@@ -0,0 +1,11 @@
+export const sampleStudents = [
+  {
+    ID: 1,
+    Name: 'Chai',
+    Gender: 'male',
+    Address: 'street',
+    MobileNo: '0771234567',
+    DateOfBirth: new Date(1996, 8, 20),
+    Age: 12,
+  },
+];
