cae348e9b33acce029adba9d5b4724aef8151fd9
primesh <primeshheshan@gmail.com>
2022-08-10 23:44:22 +0530

    enable cors

--- backend/apps/file-processing/src/main.ts
+++ backend/apps/file-processing/src/main.ts
@@ -2,7 +2,8 @@ import { NestFactory } from '@nestjs/core';
 import { FileProcessingModule } from './file-processing.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(FileProcessingModule)
+  const app = await NestFactory.create(FileProcessingModule);
+  app.enableCors();
   await app.listen(5200);
 }
 bootstrap();
