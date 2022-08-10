4e561842899a4849ced54cada20f5c6ee231a253
primesh <primeshheshan@gmail.com>
2022-08-10 23:44:06 +0530

    create student from dto

--- backend/apps/file-processing/src/file-upoad.processor.ts
+++ backend/apps/file-processing/src/file-upoad.processor.ts
@@ -4,6 +4,7 @@ import { OnQueueActive, Process, Processor } from '@nestjs/bull'
 import { Job } from "bull";
 import * as csv from 'csvtojson';
 import { Student } from "./entities/student.entity";
+import { StudentCreateDto } from "./dto/create-student.input";
 
 
 
@@ -13,23 +14,25 @@ export class UploadProcessor {
 
     @Process('csv')
     async handleCsvFiles(job: Job) {
-        const csvFilePath = process.cwd() + "\\uploads\\" + job.data.fileName;
-        const jsonArray = await csv().fromFile(csvFilePath);
-
-        for (let i = 0; i < jsonArray.length; i++) {
-            let student: Student = {
-                id: jsonArray[i].id,
-                name: jsonArray[i].name,
-                gender: jsonArray[i].gender,
-                address: jsonArray[i].address,
-                mobileNo: jsonArray[i].mobileNo,
-                dateOfBirth: jsonArray[i].dateOfBirth,
-                inEdit: jsonArray[i].inEdit,
-                age: jsonArray[i].age,
-                isArchive: jsonArray[i].isArchive
-            };
-            this.studentRepository.create(student);
-            await this.studentRepository.save(student);
+        try {
+            const csvFilePath = process.cwd() + "\\uploads\\" + job.data.fileName;
+            const jsonArray = await csv().fromFile(csvFilePath);
+            for (let i = 0; i < jsonArray.length; i++) {
+                let student: StudentCreateDto = {
+                    name: jsonArray[i].name,
+                    gender: jsonArray[i].gender,
+                    address: jsonArray[i].address,
+                    mobileNo: jsonArray[i].mobileNo,
+                    dateOfBirth: new Date(jsonArray[i].dateOfBirth),
+                    inEdit: jsonArray[i].inEdit,
+                    age: jsonArray[i].age,
+                    isArchive: jsonArray[i].isArchive
+                };
+                const newStudentObj: Student = this.studentRepository.create(student);
+                await this.studentRepository.save(newStudentObj);
+            }
+        } catch (error) {
+            console.log(error)
         }
     }
 
