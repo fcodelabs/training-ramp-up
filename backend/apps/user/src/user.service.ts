a2a98bb3b9a26b6c2ae372d28deaacc89f69abf9
primesh <primeshheshan@gmail.com>
2022-08-15 15:36:24 +0530

    hash password using bycrypt

--- backend/apps/user/src/user.service.ts
+++ backend/apps/user/src/user.service.ts
@@ -3,14 +3,20 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { UserCreateDto } from './dto/create.user.input';
 import { User } from './entities/user.entity';
-
+import * as bcrypt from 'bcrypt';
 @Injectable()
 export class UserService {
   constructor(@InjectRepository(User) private userRepository: Repository<User>) { }
 
   async create(user: UserCreateDto): Promise<User> {
-    let userObj: User = this.userRepository.create(user);
-    return await this.userRepository.save(userObj);
+    const saltRounds = 10;
+    const userObj = {
+      email: user.email,
+      password: bcrypt.hashSync(user.password, saltRounds),
+      isArchive: user.isArchive
+    }
+    const createdUserObj = this.userRepository.create(userObj);
+    return await this.userRepository.save(createdUserObj);
   }
 
   async findAll(): Promise<User[]> {
