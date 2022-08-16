75e867a8083231662299119f89f685e50f94ec92
primesh <primeshheshan@gmail.com>
2022-08-16 19:29:43 +0530

    enabale cors

--- backend/apps/auth/src/main.ts
+++ backend/apps/auth/src/main.ts
@@ -2,7 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AuthModule } from './auth.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AuthModule);
+  const app = await NestFactory.create(AuthModule, { cors: true });
   await app.listen(5400);
 }
 bootstrap();
