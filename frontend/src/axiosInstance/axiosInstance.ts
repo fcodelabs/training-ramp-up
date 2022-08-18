5d332607699e04b4444a001242062b91db465854
primesh <primeshheshan@gmail.com>
2022-08-19 00:09:23 +0530

    axios instance

---
+++ frontend/src/axiosInstance/axiosInstance.ts
@@ -0,0 +1,23 @@
+import axios from 'axios';
+
+const axiosInstance = axios.create({
+    baseURL: 'http://localhost:5400/',
+});
+
+axiosInstance.interceptors.response.use(
+    async (response) => response,
+    (error) => {
+        if (error.response.status === 401) {
+            console.log('error.response: ', error.response.status);
+            const getRefreshToken = async () => {
+                const res = await axios.get("http://localhost:5400/refresh", {
+                    withCredentials: true
+                })
+                localStorage.setItem("accessToken", res.data.access_token);
+            }
+            getRefreshToken();
+        }
+    }
+);
+
+export default axiosInstance;
