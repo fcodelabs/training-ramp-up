b1bc6f65847d4df39656e1c6d5fdbbee9a32fa01
primesh <primeshheshan@gmail.com>
2022-08-18 19:11:08 +0530

    remove accesstoken in store

--- frontend/src/store/loged-slice.tsx
+++ frontend/src/store/loged-slice.tsx
@@ -4,15 +4,10 @@ const logSlice = createSlice({
   name: 'logSlice',
   initialState: {
     isLoggedIn: false,
-    accessToken: '',
   },
   reducers: {
     toggleState: (state: any, action: any) => {
       state.isLoggedIn = action.payload;
-    },
-
-    storeAccessToken: (state: any, action: any) => {
-      state.accessToken = action.payload;
     },
   },
 });
