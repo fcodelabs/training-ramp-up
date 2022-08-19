50304c50884ce7050ad81a38b39bf5d6e59c98df
primesh <primeshheshan@gmail.com>
2022-08-19 08:28:06 +0530

    change console.logs

--- frontend/src/axiosInstance/axiosInstance.ts
+++ frontend/src/axiosInstance/axiosInstance.ts
@@ -8,7 +8,7 @@ axiosInstance.interceptors.response.use(
     async (response) => response,
     (error) => {
         if (error.response.status === 401) {
-            console.log('error.response: ', error.response.status);
+            console.log('response status: ', error.response.status);
             const getRefreshToken = async () => {
                 const res = await axios.get("http://localhost:5400/refresh", {
                     withCredentials: true
