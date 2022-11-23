092a9fe5c672dd2ab5ee80fbda846f3cf5dd9aab
abdullanalimm <abdullanalimm@gmail.com>
2022-11-23 16:50:43 +0530

    data grid with dummy data

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,33 @@
 import React from "react";
+import { students } from "./data/students";
+import { Grid, GridColumn } from "@progress/kendo-react-grid";
+import "@progress/kendo-theme-default/dist/all.css";
 import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello</h1>
-    </div>
+    <Grid style={{ height: "400px" }} data={students}>
+      <GridColumn field="id" title="ID" width="40px" />
+      <GridColumn field="name" title="Name" width="250px" />
+      <GridColumn field="gender" title="Gender" />
+      <GridColumn field="address" title="Address" />
+      <GridColumn field="mobile" title="Mobile No" />
+      <GridColumn field="dateOfBirth" title="Date of Birth" />
+      <GridColumn field="age" title="Age" />
+      <GridColumn
+        title="Command"
+        cell={() => (
+          <td className="k-command-cell">
+            <button className="k-button k-button-md k-rounded-md k-button-solid k-button-solid-primary k-grid-edit-command">
+              Edit
+            </button>
+            <button className="k-button k-button-md k-rounded-md k-button-solid k-button-solid-base k-grid-remove-command">
+              Remove
+            </button>
+          </td>
+        )}
+      />
+    </Grid>
   );
 }
 
---
+++ src/data/students.ts
@@ -0,0 +1,38 @@
+export const students = [
+    {
+        id: 1,
+        name: 'John',
+        gender: "male",
+        address: "Kohuwala",
+        mobile: "0769099876",
+        dateOfBirth: "1999-01-01",
+        age: 23,
+    },
+    {
+        id: 2,
+        name: 'Shanika',
+        gender: "fmale",
+        address: "Kottawa",
+        mobile: "0765998373",
+        dateOfBirth: "1998-11-11",
+        age: 24,
+    },
+    {
+        id: 3,
+        name: 'Nihal',
+        gender: "male",
+        address: "Kaduwela",
+        mobile: "0760292372",
+        dateOfBirth: "1999-01-01",
+        age: 20,
+    },
+    {
+        id: 4,
+        name: 'John Doe',
+        gender: "male",
+        address: "Kohuwala",
+        mobile: "0769099876",
+        dateOfBirth: "1998-12-04",
+        age: 24,
+    }
+]
