8479f4148246db87a38d0ede4dbf798290c905ea
abdullanalimm <abdullanalimm@gmail.com>
2022-11-24 14:26:03 +0530

    data grid with dummy data

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,117 @@
 import React from "react";
-import { students } from "./data/students";
-import { Grid, GridColumn } from "@progress/kendo-react-grid";
+import {
+  Grid,
+  GridColumn,
+  GridToolbar,
+  GridCellProps,
+  GridItemChangeEvent,
+} from "@progress/kendo-react-grid";
+import { MyCommandCell } from "./myCommandCell";
 import "@progress/kendo-theme-default/dist/all.css";
+import { getStudents, insertStudent, deleteItem, updateItem } from "./services";
 import "./App.css";
 
+const editField = "inEdit";
+
 function App() {
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  const [data, setData] = React.useState([] as any);
+
+  React.useEffect(() => {
+    const newStudents = getStudents();
+    setData(newStudents);
+  }, []);
+
+  const itemChange = (event: GridItemChangeEvent) => {
+    const newData = data.map((student: { id: number }) =>
+      student.id === event.dataItem.id
+        ? { ...student, [event.field || ""]: event.value }
+        : student
+    );
+
+    setData(newData);
+  };
+
+  const addNew = () => {
+    const newDataItem = { inEdit: true };
+    setData([...data, newDataItem]);
+  };
+
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  const add = (dataItem: any) => {
+    dataItem.inEdit = true;
+
+    const newData = insertStudent(dataItem);
+    setData(newData);
+  };
+
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  const remove = (dataItem: any) => {
+    const newData = [...deleteItem(dataItem)];
+    setData(newData);
+  };
+
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  const cancel = (dataItem: any) => {
+    const originalItem = getStudents().find((p) => p.id === dataItem.id);
+    const newData = data.map((student: { id: number }) =>
+      student.id === originalItem?.id ? originalItem : student
+    );
+
+    setData(newData);
+  };
+
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  const enterEdit = (dataItem: any) => {
+    setData(
+      data.map((student: { id: number }) =>
+        student.id === dataItem.id ? { ...student, inEdit: true } : student
+      )
+    );
+  };
+
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  const update = (dataItem: any) => {
+    dataItem.inEdit = false;
+    const newData = updateItem(dataItem);
+    setData(newData);
+  };
+
+  const discard = () => {
+    const newData = [...data];
+    newData.splice(0, 1);
+    setData(newData);
+  };
+
+  const CommandCell = (props: GridCellProps) => (
+    <MyCommandCell
+      {...props}
+      edit={enterEdit}
+      remove={remove}
+      add={add}
+      discard={discard}
+      update={update}
+      cancel={cancel}
+      editField={editField}
+    />
+  );
+
   return (
-    <Grid style={{ height: "400px" }} data={students}>
+    <Grid
+      style={{ height: "400px" }}
+      data={data}
+      onItemChange={itemChange}
+      editField={editField}
+    >
+      <GridToolbar>
+        <button
+          title="Add new"
+          className="k-button k-button-md k-rounded-md k-button-solid k-button-solid-base"
+          onClick={addNew}
+        >
+          Add new
+        </button>
+      </GridToolbar>
       <GridColumn field="id" title="ID" width="40px" />
       <GridColumn field="name" title="Name" width="250px" />
       <GridColumn field="gender" title="Gender" />
@@ -14,19 +119,7 @@ function App() {
       <GridColumn field="mobile" title="Mobile No" />
       <GridColumn field="dateOfBirth" title="Date of Birth" />
       <GridColumn field="age" title="Age" />
-      <GridColumn
-        title="Command"
-        cell={() => (
-          <td className="k-command-cell">
-            <button className="k-button k-button-md k-rounded-md k-button-solid k-button-solid-primary k-grid-edit-command">
-              Edit
-            </button>
-            <button className="k-button k-button-md k-rounded-md k-button-solid k-button-solid-base k-grid-remove-command">
-              Remove
-            </button>
-          </td>
-        )}
-      />
+      <GridColumn cell={CommandCell} />
     </Grid>
   );
 }
--- src/data/students.ts
+++ src/data/students.ts
@@ -1,7 +1,7 @@
 export const students = [
     {
         id: 1,
-        name: 'John',
+        name: 'Saman',
         gender: "male",
         address: "Kohuwala",
         mobile: "0769099876",
@@ -11,7 +11,7 @@ export const students = [
     {
         id: 2,
         name: 'Shanika',
-        gender: "fmale",
+        gender: "female",
         address: "Kottawa",
         mobile: "0765998373",
         dateOfBirth: "1998-11-11",
@@ -23,12 +23,12 @@ export const students = [
         gender: "male",
         address: "Kaduwela",
         mobile: "0760292372",
-        dateOfBirth: "1999-01-01",
-        age: 20,
+        dateOfBirth: "1999-04-23",
+        age: 23,
     },
     {
         id: 4,
-        name: 'John Doe',
+        name: 'Perera',
         gender: "male",
         address: "Kohuwala",
         mobile: "0769099876",
