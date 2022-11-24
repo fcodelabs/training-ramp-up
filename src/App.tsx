27979099947377a7f9b94a2c2cf671d441a23a9f
Risini1998 <risinipiyathma1998@gmail.com>
2022-11-24 10:53:37 +0530

    ITP-697

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,50 @@
-import React from 'react'
-import logo from './logo.svg'
-import './App.css'
+import * as React from 'react'
+// import * as ReactDOM from 'react-dom'
+import { Grid, GridColumn } from '@progress/kendo-react-grid'
+import persons from './persons.json'
+import { Button } from '@progress/kendo-react-buttons'
+import '@progress/kendo-theme-default/dist/all.css'
 
 const App: React.FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div
+      style={{
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'flex-start',
+        padding: '10px',
+        minHeight: '100vh'
+      }}
+    >
+      <div style={{ marginBottom: '20px' }}>
+        <Button fillMode="outline">Add New</Button>
+      </div>
+      <Grid
+        style={{ width: '100%', minHeight: '400px', maxHeight: '600px' }}
+        data={persons}
+      >
+        <GridColumn field="PersonID" title="ID" width="80px" />
+        <GridColumn field="PersonName" title="Name" width="200px" />
+        <GridColumn field="Gender" title="Gender" width="150px" />
+        <GridColumn field="Address" title="Address" width="200px" />
+        <GridColumn field="MobileNo" title="Mobile No" width="200px" />
+        <GridColumn field="DOB" title="Date Of Birth" width="220px" />
+        <GridColumn field="Age" title="Age" width="110px" />
+        <GridColumn
+          title="command"
+          width="183px"
+          cell={() => (
+            <td className="k-command-cell">
+              <Button className="k-button-solid-primary">
+                Edit
+              </Button>
+              <Button className="k-button-solid-base">
+                Remove
+              </Button>
+            </td>
+          )}
+        />
+      </Grid>
     </div>
   )
 }
