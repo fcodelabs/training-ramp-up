ee267c74bf8f39ba8004cd6f0ac25ece117b83cd
rashidya <rashmiliyanage2000@gmail.com>
2022-12-01 10:10:17 +0530

    updated

--- training-ramp-up-backend/app.ts
+++ training-ramp-up-backend/app.ts
@@ -1,19 +1,19 @@
 // Import the express in typescript file
-import express from "express";
+import express from 'express'
 
 // Initialize the express engine
-const app: express.Application = express();
+const app: express.Application = express()
 
 // Take a port 3000 for running server.
-const port: number = 3000;
+const port = 3000
 
 // Handling '/' Request
-app.get("/", (_req, _res) => {
-  _res.send("Im the get request handler");
-});
+app.get('/', (_req, _res) => {
+	_res.send('Im the get request handler')
+})
 
 // Server setup
 app.listen(port, () => {
-  console.log(`TypeScript with Express
-         http://localhost:${port}/`);
-});
+	console.log(`TypeScript with Express
+         http://localhost:${port}/`)
+})
