ee68b254060f25d361c0e1c9f91d4ec127e0ab72
abdullanalimm <abdullanalimm@gmail.com>
2022-12-05 13:47:29 +0530

    connect apis with database

--- Back-End/src/controllers/studentController.ts
+++ Back-End/src/controllers/studentController.ts
@@ -3,18 +3,23 @@ import { Request, Response } from 'express';
 import {
   addStudentService,
   getAllStudentsService,
-  updateStudentService,
+  findStudentService,
   deleteStudentService,
+  mergeStudentService,
+  saveStudentService,
 } from '../services/studentService';
 
 export const addStudent = async (req: Request, res: Response) => {
   try {
-    const student = await addStudentService(req.body).catch((err: any) => {
-      res.status(500);
-      res.json(err);
-      return;
-    });
-    res.status(200);
+    const student = await addStudentService(
+      req.body.id,
+      req.body.name,
+      req.body.gender,
+      req.body.address,
+      req.body.mobile,
+      req.body.birthday
+    );
+    res.status(201);
     res.json(student);
   } catch (err) {
     res.status(500);
@@ -38,29 +43,22 @@ export const getAllStudents = async (req: Request, res: Response) => {
 };
 
 export const updateStudent = async (req: Request, res: Response) => {
-  try {
-    const student = await updateStudentService(req.body).catch((err: any) => {
-      res.status(500);
-      res.json(err);
-      return;
-    });
-    res.status(200);
-    res.json(student);
-  } catch (err) {
-    res.status(500);
-    res.json(err);
+  const id = parseInt(req.params.id);
+  const student = await findStudentService(id);
+
+  if (student) {
+    mergeStudentService(student, req.body);
+    const results = await saveStudentService(student);
+    return res.send(results);
   }
 };
 
 export const deleteStudent = async (req: Request, res: Response) => {
   try {
-    const students = await deleteStudentService(req.body).catch((err: any) => {
-      res.status(500);
-      res.json(err);
-      return;
-    });
+    const id = parseInt(req.params.id);
+    const result = await deleteStudentService(id);
     res.status(200);
-    res.json(students);
+    res.json(result);
   } catch (err) {
     res.status(500);
     res.json(err);
--- Back-End/src/services/studentService.ts
+++ Back-End/src/services/studentService.ts
@@ -1,35 +1,49 @@
-import { Student } from '../models/student';
-const students: Array<Student> = [];
+import { BaseEntity, DeepPartial } from 'typeorm';
+import { Student } from '../entities/studentEntity';
 
-export const addStudentService = async (student: Student) => {
-  async function add(student: Student) {
-    students.push(student);
-    return student;
-  }
-  return add(student);
+// import { Student } from '../models/student';
+// const students: Array<Student> = [];
+
+export const addStudentService = async (
+  id: number,
+  name: string,
+  gender: string,
+  address: string,
+  mobile: string,
+  birthday: string
+) => {
+  return Student.create({
+    id: id,
+    name: name,
+    gender: gender,
+    address: address,
+    mobile: mobile,
+    birthday: birthday,
+  }).save();
 };
 
 export const getAllStudentsService = async () => {
   async function getAll() {
-    return students;
+    return Student.find();
   }
   return getAll();
 };
 
-export const updateStudentService = async (student: Student) => {
-  async function update(student: Student) {
-    const index = students.findIndex((s) => s.id === student.id);
-    students[index] = student;
-    return student;
-  }
-  return update(student);
+export const findStudentService = async (studentId: number) => {
+  return await Student.findOneBy({ id: studentId });
 };
 
-export const deleteStudentService = async (student: Student) => {
-  async function remove(student: Student) {
-    const index = students.findIndex((s) => s.id === student.id);
-    students.splice(index, 1);
-    return students;
+export const mergeStudentService = async (student: Student, body: DeepPartial<BaseEntity>) => {
+  return Student.merge(student, body);
+};
+
+export const saveStudentService = async (student: Student) => {
+  return await Student.save(student);
+};
+
+export const deleteStudentService = async (id: number) => {
+  async function remove(id: number) {
+    return await Student.delete({ id: id });
   }
-  return remove(student);
+  return remove(id);
 };
