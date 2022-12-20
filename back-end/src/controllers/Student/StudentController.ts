aac847a9054ffa4383793c68747d0041464b164f
Risini1998 <risinipiyathma1998@gmail.com>
2022-12-20 21:46:01 +0530

    done changes

--- back-end/src/controllers/Student/StudentController.ts
+++ back-end/src/controllers/Student/StudentController.ts
@@ -8,7 +8,7 @@ import {
   deleteStudentService
 } from '../../services/Student/StudentService'
 
-const validateUpdate = (person: StudentModel) => {
+const validate = (person: StudentModel) => {
   const name = /^([A-z\s.]{3,20})$/
 
   const address = /^([A-z0-9/,\s]{5,})$/
@@ -36,34 +36,6 @@ const validateUpdate = (person: StudentModel) => {
   return true
 }
 
-const validateInsert = (person: StudentModel) => {
-  const name = /^([A-z\s.]{3,20})$/
-
-  const address = /^([A-z0-9/,\s]{5,})$/
-
-  const mobileNo = /^([0][0-9]{9})$/
-
-  const age: number = Math.round((new Date().getTime() - new Date(person.dob).getTime()) / (1000 * 60 * 60 * 24 * 365))
-  const validateAge: boolean = age >= 18
-
-  if (!name.test(person.name)) {
-    return false
-  }
-  if (person.gender === '') {
-    return false
-  }
-  if (!address.test(person.address)) {
-    return false
-  }
-  if (!mobileNo.test(person.mobileNo)) {
-    return false
-  }
-  if (person.dob == null || !validateAge) {
-    return false
-  }
-  return true
-}
-
 export const getAllStudents = async (req: Request, res: Response) => {
   try {
     const students = await getAllStudentsService()
@@ -75,7 +47,7 @@ export const getAllStudents = async (req: Request, res: Response) => {
 
 export const addStudent = async (req: Request, res: Response) => {
   try {
-    if (validateInsert(req.body)) {
+    if (validate(req.body)) {
       const result = await addStudentService(req.body)
       res.status(201).send(result)
       io.emit(
@@ -92,7 +64,7 @@ export const addStudent = async (req: Request, res: Response) => {
 
 export const updateStudent = async (req: Request, res: Response) => {
   try {
-    if (validateUpdate(req.body)) {
+    if (validate(req.body)) {
       const result = await updateStudentService(req.body)
       res.status(200).send(result)
       io.emit(
