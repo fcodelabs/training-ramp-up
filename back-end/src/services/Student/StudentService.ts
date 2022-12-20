803b47b1a239d5080383cc9492ab71f716b38478
Risini1998 <risinipiyathma1998@gmail.com>
2022-12-20 11:39:22 +0530

    issues fixed 2

--- back-end/src/services/Student/StudentService.ts
+++ back-end/src/services/Student/StudentService.ts
@@ -10,34 +10,50 @@ DatabaseService.initialize().then(() => {
   })
 
 export const getAllStudentsService = async () => {
-  const students = await DatabaseService.getRepository(Student).find({ order: { id: 'ASC' } })
-  return students
+  try {
+    const students = await DatabaseService.getRepository(Student).find({ order: { id: 'ASC' } })
+    return students
+  } catch (err) {
+    return err
+  }
 }
 
 export const addStudentService = async (student: StudentModel) => {
-  const newStudent = new Student()
-  newStudent.name = student.name
-  newStudent.gender = student.gender
-  newStudent.address = student.address
-  newStudent.mobileNo = student.mobileNo
-  newStudent.dob = student.dob
-  const result = await DatabaseService.getRepository(Student).save(newStudent)
-  return result
+  try {
+    const newStudent = new Student()
+    newStudent.name = student.name
+    newStudent.gender = student.gender
+    newStudent.address = student.address
+    newStudent.mobileNo = student.mobileNo
+    newStudent.dob = student.dob
+    const result = await DatabaseService.getRepository(Student).save(newStudent)
+    return result
+  } catch (err) {
+    return err
+  }
 }
 
 export const updateStudentService = async (updatestudent: StudentModel) => {
-  const studentId = updatestudent.id
-  const student = await DatabaseService.getRepository(Student).findOneBy({
-    id: studentId
-  })
-  if (student !== null) {
-    DatabaseService.getRepository(Student).merge(student, updatestudent)
-    const result = await DatabaseService.getRepository(Student).save(student)
-    return result
+  try {
+    const studentId = updatestudent.id
+    const student = await DatabaseService.getRepository(Student).findOneBy({
+      id: studentId
+    })
+    if (student !== null) {
+      DatabaseService.getRepository(Student).merge(student, updatestudent)
+      const result = await DatabaseService.getRepository(Student).save(student)
+      return result
+    }
+  } catch (err) {
+    return err
   }
 }
 
 export const deleteStudentService = async (ID: number) => {
-  const result = await DatabaseService.getRepository(Student).delete(ID)
-  return result
+  try {
+    const result = await DatabaseService.getRepository(Student).delete(ID)
+    return result
+  } catch (err) {
+    return err
+  }
 }
