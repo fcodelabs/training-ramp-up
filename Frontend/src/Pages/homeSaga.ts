4eaca37cf0dca96d0fba6fd21b4ff7545b509937
Thilina Sandakelum <thilinas.19@cse.mrt.ac.lk>
2023-01-18 16:04:31 +0530

    added

--- Frontend/src/Pages/homeSaga.ts
+++ Frontend/src/Pages/homeSaga.ts
@@ -1,5 +1,5 @@
-import { take, takeEvery, put, call, cancelled } from "redux-saga/effects";
-import { User } from "../components/interface";
+import { takeEvery, put, call } from "redux-saga/effects";
+import { Student } from "../components/interface";
 import axios from "../axios";
 import {
   getStudents,
@@ -8,12 +8,18 @@ import {
   addStudent,
   addStudentSuccess,
   addStudentFailure,
+  deleteStudent,
+  deleteStudentSuccess,
+  deleteStudentFailure,
+  updateStudent,
+  updateStudentSuccess,
+  updateStudentFailure,
 } from "./homeSlice";
 
 function* getStudentsSaga(): Generator<any, any, any> {
   try {
     const response = yield call(() => axios.get("student"));
-    const students: User[] = response.data;
+    const students: Student[] = response.data;
     const modified = students.map((student) => {
       return {
         ...student,
@@ -30,7 +36,7 @@ function* addStudentSaga(action: any): Generator<any, any, any> {
   try {
     console.log("start saga", action.payload);
     const response = yield call(() => axios.post("student", action.payload));
-    const student: User = response.data;
+    const student: Student = response.data;
     console.log(student);
     yield put(addStudentSuccess(student));
   } catch (error) {
@@ -38,9 +44,38 @@ function* addStudentSaga(action: any): Generator<any, any, any> {
   }
 }
 
+function* deleteStudentSaga(action: any): Generator<any, any, any> {
+  try {
+    yield call(() => axios.delete(`student/${action.payload}`));
+    yield put(deleteStudentSuccess(action.payload));
+  } catch (error) {
+    yield put(deleteStudentFailure(error));
+  }
+}
+
+function* updateStudentSaga(action: any): Generator<any, any, any> {
+  try {
+    const element = {
+      name: action.payload.name,
+      address: action.payload.address,
+      age: action.payload.age,
+      birthday: action.payload.birthday,
+      gender: action.payload.gender,
+      mobile: action.payload.mobile,
+    };
+    yield call(() => axios.patch(`student/${action.payload.id}`, element));
+
+    yield put(updateStudentSuccess(action.payload));
+  } catch (error) {
+    yield put(updateStudentFailure(error));
+  }
+}
+
 function* homeSaga() {
   yield takeEvery(getStudents, getStudentsSaga);
   yield takeEvery(addStudent, addStudentSaga);
+  yield takeEvery(deleteStudent, deleteStudentSaga);
+  yield takeEvery(updateStudent, updateStudentSaga);
 }
 
 export default homeSaga;
