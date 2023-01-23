7351e6ca476123da9f06ffcb5ee93c61752bc58e
Nuwa-Hub <you@example.com>
2023-01-24 02:49:27 +0530

    add tsconfig.eslint file

--- frontend/.eslintrc.js
+++ frontend/.eslintrc.js
@@ -13,7 +13,10 @@ module.exports = {
   parserOptions: {
     ecmaVersion: 'latest',
     sourceType: 'module',
-    project: "tsconfig.json",
+    project: [
+       './tsconfig.json',
+      './tsconfig.eslint.json',
+    ],
     tsconfigRootDir: __dirname,
   },
   plugins: ['react', 'react-hooks', '@typescript-eslint', 'prettier'],
