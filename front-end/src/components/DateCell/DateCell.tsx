397c30f16c7606c116826de8e5a1996492a7f45a
Piyumi Chan Mahaarachchi <76487449+YumeChaan@users.noreply.github.com>
2023-01-26 20:52:27 +0530

    some bugs are fixed

---
+++ front-end/src/components/DateCell/DateCell.tsx
@@ -0,0 +1,38 @@
+import { DatePicker, DatePickerChangeEvent } from '@progress/kendo-react-dateinputs'
+import { GridCellProps } from '@progress/kendo-react-grid'
+
+export const DateCell = (props: GridCellProps) => {
+  function subtractYears(date: Date, years: number): Date {
+    date.setFullYear(date.getFullYear() - years)
+    return date
+  }
+  const today = new Date()
+  const max = subtractYears(today, 18)
+
+  const { dataItem } = props
+  const field = props.field || ''
+  const dataValue = dataItem[field] === undefined ? new Date(max) : new Date(dataItem[field])
+
+  const handleChange = (e: DatePickerChangeEvent) => {
+    if (props.onChange) {
+      props.onChange({
+        dataIndex: 0,
+        dataItem: props.dataItem,
+        field: props.field,
+        syntheticEvent: e.syntheticEvent,
+        value: e.target.value,
+      })
+    }
+  }
+  return (
+    <td>
+      {dataItem.inEdit ? (
+        <DatePicker max={max} onChange={handleChange} defaultValue={dataValue} />
+      ) : dataValue != null ? (
+        dataValue.toDateString()
+      ) : (
+        ''
+      )}
+    </td>
+  )
+}
