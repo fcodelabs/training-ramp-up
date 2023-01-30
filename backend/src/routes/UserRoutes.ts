25dddd179ca42ce0184ffdfef2c34fb78782b320
Nuwa-Hub <you@example.com>
2023-01-30 17:20:17 +0530

    change

--- backend/src/routes/UserRoutes.ts
+++ backend/src/routes/UserRoutes.ts
@@ -1,15 +1,15 @@
-import { verifyJWT } from "./../middleware/verifyJWT";
 import {
-  loginController,
-  logoutController,
   refreshTokenController,
-} from "./../controllers/userController";
+  logoutController,
+  loginController,
+  signUpController,
+} from "./../controllers/UserController";
+import { verifyJWT } from "./../middleware/verifyJWT";
 import { Router } from "express";
-import { signUpController } from "../controllers/userController";
 
 const router = Router();
 
-router.post("/signup",  signUpController);
+router.post("/signup", signUpController);
 router.post("/login", loginController);
 router.get("/refresh", refreshTokenController);
 router.get("/logout", logoutController);
