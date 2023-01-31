35df11452b5a28c7c12cbf35bd1dd7cf5bd2f217
Piyumi Chan Mahaarachchi <76487449+YumeChaan@users.noreply.github.com>
2023-01-31 16:51:30 +0530

    error

--- front-end/src/api/client.ts
+++ front-end/src/api/client.ts
@@ -4,10 +4,6 @@ export const axiosClient = axios.create({
   baseURL: 'http://localhost:8080/api/student',
 })
 export async function resolver(axiosResponse: any) {
-  try {
-    const response = await axiosResponse
-    return response
-  } catch (error) {
-    return [408, 'connectiion error']
-  }
+  const response = await axiosResponse
+  return response
 }
