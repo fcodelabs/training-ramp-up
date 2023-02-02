1ef3fa7c4dc1e4e4e76616c7bd7258f678b2f77e
Piyumi Chan Mahaarachchi <76487449+YumeChaan@users.noreply.github.com>
2023-02-02 19:19:30 +0530

    joi validations

--- front-end/src/pages/SignUp/SignUp.tsx
+++ front-end/src/pages/SignUp/SignUp.tsx
@@ -1,5 +1,6 @@
 import * as React from 'react'
 import Avatar from '@mui/material/Avatar'
+import joi from 'joi'
 import Button from '@mui/material/Button'
 import CssBaseline from '@mui/material/CssBaseline'
 import TextField from '@mui/material/TextField'
@@ -9,21 +10,102 @@ import Grid from '@mui/material/Grid'
 import Box from '@mui/material/Box'
 import Typography from '@mui/material/Typography'
 import Container from '@mui/material/Container'
+import Alert from '@mui/material/Alert'
 
 import { createTheme, ThemeProvider } from '@mui/material/styles'
-
-
+import { useState } from 'react'
 
 const theme = createTheme()
-
 export default function SignUp() {
+  interface IBuyer {
+    email: string
+    password: string
+    confPassword: string
+  }
+
+  const [user, setUser] = useState<IBuyer>({
+    email: '',
+    password: '',
+    confPassword: '',
+  })
+
+  const schema: { [key: string]: any } = joi.object({
+    email: joi
+      .string()
+      .email({ tlds: { allow: false } })
+      .required(),
+    password: joi
+      .string()
+      .min(8)
+      .max(25)
+      .regex(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/, 'password')
+      .required(),
+    confPassword: joi
+      .any()
+      .equal(joi.ref('password'))
+      .required()
+      .label('Confirm password')
+      .options({ messages: { 'any.only': '{{#label}} does not match' } }),
+  })
+
+  interface IErrors {
+    [key: string]: string
+  }
+  const [errors, setErrors] = useState<IErrors>({})
+
+  const handleSave = (event: React.ChangeEvent<HTMLTextAreaElement>): void => {
+    const { name, value } = event.target
+    const errorData = { ...errors }
+    const errorMessage = validateProperty(event)
+    if (errorMessage) {
+      errorData[name] = errorMessage
+    } else {
+      delete errorData[name]
+    }
+    const UserData: any = { ...user }
+    UserData[name] = value
+    setUser(UserData)
+    setErrors(errorData)
+  }
+
+  const validateProperty = (event: React.ChangeEvent<HTMLTextAreaElement>) => {
+    const { name, value } = event.target
+    if (name === 'confPassword') {
+      const obj = { password: user.password, [name]: value }
+      const subSchema = joi.object({
+        [name]: schema.extract(name),
+        password: schema.extract('password'),
+      })
+      const { error } = subSchema.validate(obj)
+      return error ? error.details[0].message : null
+    } else {
+      const obj = { [name]: value }
+      const subSchema = joi.object({
+        [name]: schema.extract(name),
+      })
+      const result = subSchema.validate(obj)
+      const { error } = result
+      return error ? error.details[0].message : null
+    }
+  }
+
   const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault()
-    const data = new FormData(event.currentTarget)
-    console.log({
-      email: data.get('email'),
-      password: data.get('password'),
-    })
+    const result = schema.validate(user)
+    const { error } = result
+    if (!error) {
+      console.log('Submitted')
+    } else {
+      const errorData: any = {}
+      for (const item of error.details) {
+        const name = item.path[0]
+        const message = item.message
+        errorData[name] = message
+      }
+      setErrors(errorData)
+      console.log(errorData)
+      return errorData
+    }
   }
 
   return (
@@ -53,8 +135,16 @@ export default function SignUp() {
                   id='email'
                   label='Email Address'
                   name='email'
+                  value={user.email}
+                  onChange={handleSave}
                   autoComplete='email'
                 />
+
+                {errors.email && (
+                  <Alert sx={{ mt: '1vw', mb: '1vw' }} severity='error'>
+                    Invalid Email
+                  </Alert>
+                )}
               </Grid>
               <Grid item xs={12}>
                 <TextField
@@ -64,8 +154,15 @@ export default function SignUp() {
                   label='Password'
                   type='password'
                   id='password'
+                  value={user.password}
+                  onChange={handleSave}
                   autoComplete='new-password'
                 />
+                {errors.password && (
+                  <Alert sx={{ mt: '1vw', mb: '1vw' }} severity='error'>
+                    Invalid Password
+                  </Alert>
+                )}
               </Grid>
               <Grid item xs={12}>
                 <TextField
@@ -75,8 +172,15 @@ export default function SignUp() {
                   label='Confirm Password'
                   type='password'
                   id='confPassword'
+                  value={user.confPassword}
+                  onChange={handleSave}
                   autoComplete='conf-password'
                 />
+                {errors.confPassword && (
+                  <Alert sx={{ mt: '1vw', mb: '1vw' }} severity='error'>
+                    Password does not match
+                  </Alert>
+                )}
               </Grid>
             </Grid>
             <Button type='submit' fullWidth variant='contained' sx={{ mt: 3, mb: 2 }}>
@@ -91,7 +195,6 @@ export default function SignUp() {
             </Grid>
           </Box>
         </Box>
-
       </Container>
     </ThemeProvider>
   )
--- front-end/src/routes/router.tsx
+++ front-end/src/routes/router.tsx
@@ -3,6 +3,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import SignIn from '../pages/SignIn/SignIn'
 import Grid from '../pages/Grid/Grid'
 import SignUp from '../pages/SignUp/SignUp'
+import Test from '../pages/Test/Test'
 import { ToastContainer } from 'react-toastify'
 
 export default function AppRouter() {
@@ -12,6 +13,7 @@ export default function AppRouter() {
         <Route path='/' element={<SignIn />} />
         <Route path='/grid' element={<Grid />} />
         <Route path='/signup' element={<SignUp />} />
+        <Route path='/test' element={<Test />} />
       </Routes>
       <ToastContainer />
     </BrowserRouter>
