672c149c428b30085a83b4e3b9db0aa8927c9958
Piyumi Chan Mahaarachchi <76487449+YumeChaan@users.noreply.github.com>
2023-03-02 01:57:07 +0530

    user service tests done

--- nest-backend/src/users/users.service.spec.ts
+++ nest-backend/src/users/users.service.spec.ts
@@ -1,18 +1,83 @@
+import { HttpException } from '@nestjs/common';
 import { Test, TestingModule } from '@nestjs/testing';
+import { Repository } from 'typeorm';
+import { getRepositoryToken } from '@nestjs/typeorm';
 import { UsersService } from './users.service';
+import { User } from './entities/user.entity';
+
+const bcrypt = require('bcrypt');
 
 describe('UsersService', () => {
   let service: UsersService;
+  let userRepository: Repository<User>;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [UsersService],
+      providers: [
+        UsersService,
+        {
+          provide: getRepositoryToken(User),
+          useClass: class MockRepository extends Repository<User> {},
+        },
+      ],
     }).compile();
 
     service = module.get<UsersService>(UsersService);
+    userRepository = module.get<Repository<User>>(getRepositoryToken(User));
   });
 
-  it('should be defined', () => {
-    expect(service).toBeDefined();
+  describe('signUp', () => {
+    it('should create a new user with a hashed password', async () => {
+      const createUserDto = { email: 'test@example.com', password: 'password' };
+      const hash =
+        '$2b$10$wv6jB/E79NV0Mfv2JW0qxOtuCMN1KjtpSl6eNXU6C.KU6vq3qoEyy'; // bcrypt hash for 'password'
+      jest.spyOn(bcrypt, 'hash').mockReturnValueOnce(hash);
+      jest.spyOn(userRepository, 'findOne').mockResolvedValueOnce(undefined);
+      jest
+        .spyOn(userRepository, 'create')
+        .mockReturnValueOnce(createUserDto as User);
+      jest
+        .spyOn(userRepository, 'save')
+        .mockResolvedValueOnce(createUserDto as User);
+
+      const user = await service.signUp(createUserDto);
+
+      expect(user).toEqual(createUserDto);
+      expect(bcrypt.hash).toHaveBeenCalledWith('password', 10);
+      expect(userRepository.findOne).toHaveBeenCalledWith({
+        where: { email: 'test@example.com' },
+      });
+      expect(userRepository.create).toHaveBeenCalledWith(createUserDto);
+      expect(userRepository.save).toHaveBeenCalledWith(createUserDto);
+    });
+
+    it('should throw an HttpException if the user already exists', async () => {
+      const createUserDto = { email: 'test@example.com', password: 'password' };
+      jest
+        .spyOn(userRepository, 'findOne')
+        .mockResolvedValueOnce(createUserDto as User);
+
+      await expect(service.signUp(createUserDto)).rejects.toThrow(
+        HttpException,
+      );
+
+      expect(userRepository.findOne).toHaveBeenCalledWith({
+        where: { email: 'test@example.com' },
+      });
+    });
+
+    it('should rethrow any caught errors as an HttpException', async () => {
+      const createUserDto = { email: 'test@example.com', password: 'password' };
+      const error = new Error('Database connection error');
+      jest.spyOn(userRepository, 'findOne').mockRejectedValueOnce(error);
+
+      await expect(service.signUp(createUserDto)).rejects.toThrow(
+        HttpException,
+      );
+
+      expect(userRepository.findOne).toHaveBeenCalledWith({
+        where: { email: 'test@example.com' },
+      });
+    });
   });
 });
