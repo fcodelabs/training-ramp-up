c31825ee47650c023b8bd14b21909c808e8904a4
Pasan Pahasara <pasanpahasara7788@gmail.com>
2023-10-27 10:38:00 +0530

    FullyRefactored HomePage.tsx

--- web_front_end/src/containers/HomePage/HomePage.tsx
+++ web_front_end/src/containers/HomePage/HomePage.tsx
@@ -11,87 +11,69 @@ export default function HomePage() {
     const currentUserRole = useSelector(
         (state: RootState) => state.userDataList.currentUserRole,
     );
+    const isAdmin = currentUserRole === ERoleEnum.ADMIN;
 
-    const [showStudentDataGrid, setShowStudentDataGrid] = useState(
-        currentUserRole == ERoleEnum.USER ? false : true,
-    );
-    const [showUserDataGrid, setShowUserDataGrid] = useState(
-        currentUserRole == ERoleEnum.USER ? true : false,
-    );
-
-    const [selectedButton, setSelectedButton] = useState(0);
+    const [selectedButton, setSelectedButton] = useState(isAdmin ? 0 : 1);
 
     return (
         <>
             <Header />
             <Box display="flex" justifyContent="start" paddingTop="5em">
-                {currentUserRole == ERoleEnum.ADMIN && (
-                    <>
-                        <Box
+                {isAdmin && (
+                    <Box
+                        sx={{
+                            display: "flex",
+                            flexDirection: "row",
+                            justifyContent: "start",
+                            m: 2,
+                            borderRadius: "30px",
+                            backgroundColor: "#ECF0F1",
+                            boxShadow: "0px 4px 4px rgba(0, 0, 0, 0.25)",
+                        }}
+                    >
+                        <Button
                             sx={{
-                                display: "flex",
-                                flexDirection: "row",
-                                justifyContent: "start",
-                                m: 2,
-                                borderRadius: "30px",
-                                backgroundColor: "#ECF0F1",
-                                boxShadow: "0px 4px 4px rgba(0, 0, 0, 0.25)",
+                                backgroundColor:
+                                    selectedButton === 0
+                                        ? "#44BCE7"
+                                        : "initial",
+                                color: selectedButton === 0 ? "white" : "gray",
+                                "&:hover": {
+                                    backgroundColor: "#D7E2FF",
+                                    color: "gray",
+                                },
+                                borderTopLeftRadius: "30px",
+                                borderBottomLeftRadius: "30px",
+                                fontFamily: "Ubuntu, sans-serif",
+                                fontWeight: "600",
+                                padding: "0.5rem 1rem 0.5rem 1rem",
                             }}
+                            onClick={() => setSelectedButton(0)}
                         >
-                            <Button
-                                sx={{
-                                    backgroundColor:
-                                        selectedButton === 0
-                                            ? "#44BCE7"
-                                            : "initial",
-                                    color:
-                                        selectedButton === 0 ? "white" : "gray",
-                                    "&:hover": {
-                                        backgroundColor: "#D7E2FF",
-                                        color: "gray",
-                                    },
-                                    borderTopLeftRadius: "30px",
-                                    borderBottomLeftRadius: "30px",
-                                    fontFamily: "Ubuntu, sans-serif",
-                                    fontWeight: "600",
-                                    padding: "0.5rem 1rem 0.5rem 1rem",
-                                }}
-                                onClick={() => {
-                                    setShowStudentDataGrid(true);
-                                    setShowUserDataGrid(false);
-                                    setSelectedButton(0);
-                                }}
-                            >
-                                Student Manage
-                            </Button>
-                            <Button
-                                sx={{
-                                    backgroundColor:
-                                        selectedButton === 1
-                                            ? "#44BCE7"
-                                            : "initial",
-                                    color:
-                                        selectedButton === 1 ? "white" : "gray",
-                                    "&:hover": {
-                                        backgroundColor: "#D7E2FF",
-                                        color: "gray",
-                                    },
-                                    borderTopRightRadius: "30px",
-                                    borderBottomRightRadius: "30px",
-                                    fontFamily: "Ubuntu, sans-serif",
-                                    fontWeight: "600",
-                                    padding: "0.5rem 1rem 0.5rem 1rem",
-                                }}
-                                onClick={() => {
-                                    setShowStudentDataGrid(false);
-                                    setShowUserDataGrid(true);
-                                    setSelectedButton(1);
-                                }}
-                            >
-                                User Manage
-                            </Button>
-                        </Box>
-                    </>
+                            Student Manage
+                        </Button>
+                        <Button
+                            sx={{
+                                backgroundColor:
+                                    selectedButton === 1
+                                        ? "#44BCE7"
+                                        : "initial",
+                                color: selectedButton === 1 ? "white" : "gray",
+                                "&:hover": {
+                                    backgroundColor: "#D7E2FF",
+                                    color: "gray",
+                                },
+                                borderTopRightRadius: "30px",
+                                borderBottomRightRadius: "30px",
+                                fontFamily: "Ubuntu, sans-serif",
+                                fontWeight: "600",
+                                padding: "0.5rem 1rem 0.5rem 1rem",
+                            }}
+                            onClick={() => setSelectedButton(1)}
+                        >
+                            User Manage
+                        </Button>
+                    </Box>
                 )}
             </Box>
             <Box
@@ -103,8 +85,8 @@ export default function HomePage() {
                     padding: "0 50px 0 50px",
                 }}
             >
-                {showStudentDataGrid && <StudentDataGrid />}
-                {showUserDataGrid && <UserDataGrid />}
+                {selectedButton === 0 && <StudentDataGrid />}
+                {selectedButton === 1 && <UserDataGrid />}
             </Box>
         </>
     );
