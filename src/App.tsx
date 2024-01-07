9a58d10f2e161be8c56f95d39370ead51e4a5ae8
Pasindu Madusha <pasindumadusha.20@cse.mrt.ac.lk>
2024-01-07 13:41:17 +0530

    IN-189

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import AdminPage from './containers/AdminPage/AdminPage';
+
+function App() {
+  return (
+    <div className="App">
+      <AdminPage />
+    </div>
+  );
+}
+
+export default App;
