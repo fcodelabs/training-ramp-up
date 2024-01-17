2c4131681ca048dbd3b8ade3dfb2cb18b8bd0906
Thambara Sahassaka <114559191+Thambara-20@users.noreply.github.com>
2024-01-17 17:24:12 +0530

    created student entity

--- backend/src/data-source.ts
+++ backend/src/data-source.ts
@@ -1,7 +1,7 @@
 import "reflect-metadata";
 import { DataSource } from "typeorm";
-import { User } from "./entity/User";
-import * as dotenv from 'dotenv';
+import { Student } from "./entity/Student";
+import * as dotenv from "dotenv";
 dotenv.config();
 
 export const AppDataSource = new DataSource({
@@ -13,7 +13,7 @@ export const AppDataSource = new DataSource({
   database: process.env.PG_DATABASE,
   synchronize: true,
   logging: false,
-  entities: [User],
+  entities: [Student],
   migrations: [],
   subscribers: [],
 });
--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,29 +1,51 @@
-import * as express from "express"
-import * as bodyParser from "body-parser"
-import { Request, Response } from "express"
-import { AppDataSource } from "./data-source"
-import { Routes } from "./routes"
+import * as express from "express";
+import * as bodyParser from "body-parser";
+import { Request, Response } from "express";
+import { AppDataSource } from "./data-source";
+import { Routes } from "./routes";
+import { Student } from "./entity/Student";
 
-AppDataSource.initialize().then(async () => {
+AppDataSource.initialize()
+  .then(async () => {
+    const app = express();
+    app.use(bodyParser.json());
 
-    // create express app
-    const app = express()
-    app.use(bodyParser.json())
+    Routes.forEach((route) => {
+      (app as any)[route.method](
+        route.route,
+        (req: Request, res: Response, next: Function) => {
+          const result = new (route.controller as any)()[route.action](
+            req,
+            res,
+            next
+          );
+          if (result instanceof Promise) {
+            result.then((result) =>
+              result !== null && result !== undefined
+                ? res.send(result)
+                : undefined
+            );
+          } else if (result !== null && result !== undefined) {
+            res.json(result);
+          }
+        }
+      );
+    });
 
-    Routes.forEach(route => {
-        (app as any)[route.method](route.route, (req: Request, res: Response, next: Function) => {
-            const result = (new (route.controller as any))[route.action](req, res, next)
-            if (result instanceof Promise) {
-                result.then(result => result !== null && result !== undefined ? res.send(result) : undefined)
+    app.listen(3000);
 
-            } else if (result !== null && result !== undefined) {
-                res.json(result)
-            }
-        })
-    })
+    console.log("Express server has started on port 3000. ");
 
-    app.listen(3000)
-
-    console.log("Express server has started on port 3000. ")
-
-}).catch(error => console.log(error))
+    AppDataSource.getRepository(Student)
+      .save({
+        name: "John Doe",
+        gender: "Male",
+        address: "No. 1, Galle Road, Colombo 03",
+        mobile: "0716272786",
+        birthday: new Date("1990-01-01"),
+        age: 30,
+      })
+      .then((student) => console.log("Student has been saved: ", student))
+      .catch((error) => console.log(error))
+  })
+  .catch((error) => console.log(error));
