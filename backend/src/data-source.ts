86d333836ef8d6124d4b02b798c7adc90cfe0b40
Thambara Sahassaka <114559191+Thambara-20@users.noreply.github.com>
2024-01-17 17:01:30 +0530

    updated db configurations

--- backend/src/data-source.ts
+++ backend/src/data-source.ts
@@ -1,14 +1,16 @@
 import "reflect-metadata";
 import { DataSource } from "typeorm";
 import { User } from "./entity/User";
+import * as dotenv from 'dotenv';
+dotenv.config();
 
 export const AppDataSource = new DataSource({
   type: "postgres",
-  host: "localhost",
-  port: 5432,
-  username: "postgres",
-  password: "200551R",
-  database: "postgres",
+  host: process.env.PG_HOST,
+  port: parseInt(process.env.PG_PORT),
+  username: process.env.PG_USERNAME,
+  password: process.env.PG_PASSWORD,
+  database: process.env.PG_DATABASE,
   synchronize: true,
   logging: false,
   entities: [User],
--- backend/src/index.ts
+++ backend/src/index.ts
@@ -3,7 +3,6 @@ import * as bodyParser from "body-parser"
 import { Request, Response } from "express"
 import { AppDataSource } from "./data-source"
 import { Routes } from "./routes"
-import { User } from "./entity/User"
 
 AppDataSource.initialize().then(async () => {
 
@@ -11,7 +10,6 @@ AppDataSource.initialize().then(async () => {
     const app = express()
     app.use(bodyParser.json())
 
-    // register express routes from defined application routes
     Routes.forEach(route => {
         (app as any)[route.method](route.route, (req: Request, res: Response, next: Function) => {
             const result = (new (route.controller as any))[route.action](req, res, next)
@@ -24,32 +22,8 @@ AppDataSource.initialize().then(async () => {
         })
     })
 
-    // setup express app here
-    // ...
-    
-
-
-
-    // start express server
     app.listen(3000)
 
-    // insert new users for test
-    await AppDataSource.manager.save(
-        AppDataSource.manager.create(User, {
-            firstName: "Timber",
-            lastName: "Saw",
-            age: 27
-        })
-    )
-
-    await AppDataSource.manager.save(
-        AppDataSource.manager.create(User, {
-            firstName: "Phantom",
-            lastName: "Assassin",
-            age: 24
-        })
-    )
-
-    console.log("Express server has started on port 3000. Open http://localhost:3000/users to see results")
+    console.log("Express server has started on port 3000. ")
 
 }).catch(error => console.log(error))
