979c5d00e6151ae9d6a3aadcd52816f4852925df
Pasindu Madusha <pasindumadusha.20@cse.mrt.ac.lk>
2024-01-19 15:10:33 +0530

    IN-335 <get all students>

--- backend/src/controllers/studentController.ts
+++ backend/src/controllers/studentController.ts
@@ -1,32 +1,55 @@
 import { Student } from "../models/Student";
 import { Request, Response } from "express";
 import { AppDataSource } from "../index";
+import StudentService from "../services/studentService";
 
-export const addNewStudent = async (req: Request, res: Response) => {
+const StudentController = {
+  addNewStudentController: async (req: Request, res: Response) => {
     try {
-
-        console.log("hello 1")
-        const studentRepository = AppDataSource.getRepository(Student);
-        // Create a new student instance with data from the request body
-        console.log("hello 2") 
-        const newStudent = studentRepository.create(req.body);
-        console.log("hello 3") 
-        console.log(req.body)
-        console.log(newStudent);
-
-        // Save the new student to the database
-        const savedStudent = await studentRepository.save(newStudent);
-
-        // Send the saved student data as the response
-        res.status(201).json(savedStudent);
+      console.log("hello 1");
+      //const studentRepository = AppDataSource.getRepository(Student);
+      // Create a new student instance with data from the request body
+      //console.log("hello 2");
+      StudentService.addNewStudentService(req, res);
     } catch (error) {
-        // Log the error for debugging purposes
-        console.log(req.body)
-        console.error("Error adding a new student:", error);
+      // Log the error for debugging purposes
+      console.log(req.body);
+      console.error("Error adding a new student:", error);
 
-        // Send an error response with a 500 Internal Server Error status code
-        res.status(500).json({ error: "Internal Server Error" });
+      // Send an error response with a 500 Internal Server Error status code
+      res.status(500).json({ error: "Internal Server Error" });
     }
+  },
+
+  getAllStudentsController: async (req: Request, res: Response) => {
+    try {
+      StudentService.getAllStudentsService(req, res);
+    } catch (error) {
+      console.log(req.body);
+      console.error("Error getting all students:", error);
+      res.status(500).json({ error: "Internal Server Error" });
+    }
+  },
+
+  editStudentController: async (req: Request, res: Response) => {
+    try {
+      StudentService.editStudentService(req, res);
+    } catch (error) {
+      console.log(req.body);
+      console.error("Error editing student:", error);
+      res.status(500).json({ error: "Internal Server Error" });
+    }
+  },
+
+  deleteStudentController: async (req: Request, res: Response) => {
+    try {
+      StudentService.deleteStudentService(req, res);
+    } catch (error) {
+      console.log(req.body);
+      console.error("Error deleting student:", error);
+      res.status(500).json({ error: "Internal Server Error" });
+    }
+  },
 };
 
-
+export default StudentController;
--- backend/src/routes/studentRoute.ts
+++ backend/src/routes/studentRoute.ts
@@ -1,9 +1,12 @@
 import express from "express";
-import { addNewStudent } from "../controllers/studentController";
+import StudentController from "../controllers/studentController";
 
 const router = express.Router();
 
 // Create a new student
-router.post("/add", addNewStudent);
+router.post("/add", StudentController.addNewStudentController);
+router.get("/allStudents", StudentController.getAllStudentsController);
+router.put("/edit/:id", StudentController.editStudentController);
+router.delete("/delete/:id", StudentController.deleteStudentController);
 
 export default router;
