33519254c3023403b68399bddd1009982d0c7500
Thambara Sahassaka <114559191+Thambara-20@users.noreply.github.com>
2024-01-21 20:11:02 +0530

    updated action buttons

--- frontend/src/containers/Home/Table/TableColumns/ActionColumn/ActionColumn.tsx
+++ frontend/src/containers/Home/Table/TableColumns/ActionColumn/ActionColumn.tsx
@@ -1,88 +1,88 @@
-import * as React from 'react'
-import Button from '@mui/material/Button'
-import { GridActionsCellItem, GridRenderEditCellParams } from '@mui/x-data-grid'
-import styled from 'styled-components'
-import { Box } from '@mui/material'
+import * as React from "react";
+import Button from "@mui/material/Button";
+import {
+  GridActionsCellItem,
+  GridRenderEditCellParams,
+} from "@mui/x-data-grid";
+import styled from "styled-components";
+import { Box } from "@mui/material";
 
 interface Props {
-    params: GridRenderEditCellParams
-    isInEditMode: boolean
-    handleSaveClick: () => void
-    handleCancelClick: () => void
-    handleEditClick: () => void
-    handleDeleteClick: () => void
+  params: GridRenderEditCellParams;
+  isInEditMode: boolean;
+  handleSaveClick: () => void;
+  handleCancelClick: () => void;
+  handleEditClick: () => void;
+  handleDeleteClick: () => void;
 }
 
 const EditButtonWrapper = styled.div`
-    display: flex;
-    flex-direction: column;
-    align-items: flex-start;
-`
+  display: flex;
+  height: 80%;
+  flex-direction: column;
+  justify-content: space-around;
+  align-items: flex-start;
+`;
+
+const ButtonWrapper = styled.div`
+  display: flex;
+  width: 90%;
+  flex-direction: row;
+  justify-content: space-around;
+  align-items: start;
+`;
 
 const GridActionsColumn: React.FC<Props> = ({
-    params,
-    isInEditMode,
-    handleSaveClick,
-    handleCancelClick,
-    handleEditClick,
-    handleDeleteClick,
+  params,
+  isInEditMode,
+  handleSaveClick,
+  handleCancelClick,
+  handleEditClick,
+  handleDeleteClick,
 }) => {
-    if (isInEditMode) {
-        const isNew = params.row.isNew
-        return (
-            <EditButtonWrapper>
-                <GridActionsCellItem
-                    icon={
-                        <Button variant="outlined" size="small">
-                            {isNew ? 'Add' : 'Update'}
-                        </Button>
-                    }
-                    label="Save"
-                    sx={{
-                        color: 'primary.main',
-                    }}
-                    onClick={handleSaveClick}
-                />
-                <GridActionsCellItem
-                    icon={
-                        <Button variant="outlined" size="small" color="error">
-                            Discard Changes
-                        </Button>
-                    }
-                    label="Cancel"
-                    className="textPrimary"
-                    onClick={handleCancelClick}
-                    color="inherit"
-                />
-            </EditButtonWrapper>
-        )
-    }
-
+  if (isInEditMode) {
+    const isNew = params.row.isNew;
     return (
-        <Box>
-            <GridActionsCellItem
-                icon={
-                    <Button variant="outlined" size="small">
-                        Edit
-                    </Button>
-                }
-                label="Edit"
-                className="textPrimary"
-                onClick={handleEditClick}
-                color="inherit"
-            />
-            <GridActionsCellItem
-                icon={
-                    <Button variant="outlined" size="small" color="error">
-                        Remove
-                    </Button>
-                }
-                label="Remove"
-                onClick={handleDeleteClick}
-                color="inherit"
-            />
-        </Box>
-    )
-}
+      <EditButtonWrapper>
+        <div>
+          <Button variant="outlined" size="small" onClick={handleSaveClick}>
+            <div>{isNew ? "Add" : "Update"}</div>
+          </Button>
+        </div>
 
-export default GridActionsColumn
+        <div>
+          <Button
+            variant="outlined"
+            size="small"
+            color="error"
+            onClick={handleCancelClick}
+          >
+            Discard Changes
+          </Button>
+        </div>
+      </EditButtonWrapper>
+    );
+  }
+
+  return (
+    <ButtonWrapper>
+      <div>
+        <Button variant="outlined" size="small" onClick={handleEditClick}>
+          Edit
+        </Button>
+      </div>
+      <div>
+        <Button
+          variant="outlined"
+          size="small"
+          color="error"
+          onClick={handleDeleteClick}
+        >
+          Remove
+        </Button>
+      </div>
+    </ButtonWrapper>
+  );
+};
+
+export default GridActionsColumn;
