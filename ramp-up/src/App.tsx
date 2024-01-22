9aa5571a8ea4e5fd0794872920981f42bee1e60e
Nethmini17 <muthuhewaamnk.20@uom.lk>
2024-01-22 09:26:34 +0530

    Fixed some issues

--- ramp-up/src/App.tsx
+++ ramp-up/src/App.tsx
@@ -1,8 +1,6 @@
 import DataGrids from './components/DataGrids/DataGrids'
 import TopBar from './components/TopBar/TopBar'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-import Grid from './containers/Grid/Grid'
-import DialogBox from './components/DialogBox/DialogBox'
+
 
 function App() {
   return (
