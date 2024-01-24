e1135600eaaeafab338ab7b2c41302fe1d4b4d56
Thambara Sahassaka <114559191+Thambara-20@users.noreply.github.com>
2024-01-24 22:08:38 +0530

    updated module names

---
+++ backend/src/config/data-source.ts
@@ -0,0 +1,18 @@
+import "reflect-metadata";
+import { DataSource } from "typeorm";
+import { Student } from "../entity/student";
+import * as dotenv from "dotenv";
+dotenv.config();
+
+export const AppDataSource = new DataSource({
+  url: process.env.PG_DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false,
+  },
+  type: "postgres",
+  synchronize: true,
+  logging: false,
+  entities: [Student],
+  migrations: [],
+  subscribers: [],
+});
