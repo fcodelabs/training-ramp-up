958fceedccbd5a5a1ee435e36896ea5f8d275ee6
Nethmini17 <muthuhewaamnk.20@uom.lk>
2024-02-01 01:15:31 +0530

    some minor changes

--- ts-postgre-server/src/routes/Student.ts
+++ ts-postgre-server/src/routes/Student.ts
@@ -1,16 +1,17 @@
-// src/routes/studentRoutes.ts
+//src/routes/studentRoutes.ts
 import express,{Router} from 'express';
-import { getStudents } from '../controllers/Student';
+import { createStudent, getStudents, updateStudent } from '../controllers/Student';
 import { createStudentValidation } from '../middlewares/expressValidator/createStudentValidation';
+import { deleteStudent } from '../controllers/Student';
 
 const router = express.Router();
 
 
 
 router.get('/', getStudents);
-// router.post('/', createStudentValidation, createStudent);
-// router.put('/:id', createStudentValidation, updateStudent);
-// router.delete('/:id', deleteStudent);
+router.post('/', createStudentValidation, createStudent);
+router.put('/:id', createStudentValidation, updateStudent);
+router.delete('/:id', deleteStudent);
 
 
 
@@ -18,15 +19,15 @@ router.get('/', getStudents);
 function socketRouter(io:any): Router {
     const router = Router();
   
-    // router.post("/", async (req, res) => {
-    //   try {
-    //     await createStudent(req, res).then(() => {
-    //       io.emit("newstudent", res.statusCode);
-    //     });
-    //   } catch (error) {
-    //     console.error(error);
-    //   }
-    // });
+    router.post("/", async (req, res) => {
+      try {
+        await createStudent(req, res).then(() => {
+          io.emit("newstudent", res.statusCode);
+        });
+      } catch (error) {
+        console.error(error);
+      }
+    });
   
     router.get("/", async (req, res) => {
       try {
@@ -38,25 +39,25 @@ function socketRouter(io:any): Router {
       }
     });
   
-    // router.put("/:id", async (req, res) => {
-    //   try {
-    //     await updateStudent(req, res).then(() => {
-    //       io.emit("editstudent", res.statusCode);
-    //     });
-    //   } catch (error) {
-    //     console.error(error);
-    //   }
-    // });
+    router.put("/:id", async (req, res) => {
+      try {
+        await updateStudent(req, res).then(() => {
+          io.emit("editstudent", res.statusCode);
+        });
+      } catch (error) {
+        console.error(error);
+      }
+    });
   
-    // router.delete("/:id", async (req, res) => {
-    //   try {
-    //     await deleteStudent(req, res).then(() => {
-    //       io.emit("deletestudent", res.statusCode);
-    //     });
-    //   } catch (error) {
-    //     console.error(error);
-    //   }
-    // });
+    router.delete("/:id", async (req, res) => {
+      try {
+        await deleteStudent(req, res).then(() => {
+          io.emit("deletestudent", res.statusCode);
+        });
+      } catch (error) {
+        console.error(error);
+      }
+    });
   
     return router;
   }
