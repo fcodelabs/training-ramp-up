3851223e9b823445e655276a50166c46ebfa9e57
Thambara Sahassaka <114559191+Thambara-20@users.noreply.github.com>
2024-02-06 00:39:10 +0530

    updated

--- backend/src/controllers/userController.ts
+++ backend/src/controllers/userController.ts
@@ -30,8 +30,9 @@ export class UserController {
       res
         .cookie("token", token, {
           httpOnly: true,
-          domain: "localhost",
-          secure: true,
+          domain: ".lbmsalpha.live",
+          secure: false,
+          sameSite: "lax",
           maxAge: 1000 * 60 * 60 * 5,
         })
         .status(200)
