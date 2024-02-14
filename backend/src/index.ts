4bb2fb9c02e11e003c3eef839884d429dda6135b
Pasindu Madusha <pasindumadusha.20@cse.mrt.ac.lk>
2024-02-14 09:41:08 +0530

    deployment

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -29,15 +29,36 @@ dotenv.config();
 // console.log("Random Secret Key:", secretKey);
 const app: express.Application = express();
 // app.use(cors({ origin: "http://localhost:3000", credentials: true }));
-app.use(
-  cors({ origin: "https://training-ramp-up.web.app", credentials: true }),
-);
+// app.use(
+//   cors({ origin: "https://training-ramp-up.web.app", credentials: true }),
+// );
+// app.use((req, res, next) => {
+//   res.header("Access-Control-Allow-Origin", "https://training-ramp-up.web.app");
+//   res.header("Access-Control-Allow-Credentials", "true");
+//   next();
+// });
+
+// Set middleware of CORS
 app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "https://training-ramp-up.web.app");
-  res.header("Access-Control-Allow-Credentials", "true");
+  res.setHeader(
+    "Access-Control-Allow-Origin",
+    "https://training-ramp-up.web.app",
+  );
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS,CONNECT,TRACE",
+  );
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Content-Type, Authorization, X-Content-Type-Options, Accept, X-Requested-With, Origin, Access-Control-Request-Method, Access-Control-Request-Headers",
+  );
+  res.setHeader("Access-Control-Allow-Credentials", "true");
+  res.setHeader("Access-Control-Allow-Private-Network", "true");
+  //  Firefox caps this at 24 hours (86400 seconds). Chromium (starting in v76) caps at 2 hours (7200 seconds). The default value is 5 seconds.
+  res.setHeader("Access-Control-Max-Age", 7200);
+
   next();
 });
-
 const port = process.env.PORT || 8000;
 const server = http.createServer(app);
 
