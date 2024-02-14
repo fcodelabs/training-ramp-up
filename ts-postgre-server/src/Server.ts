2cede094bba50fbd7ceb8121ab6164150f2d325e
Nethmini17 <muthuhewaamnk.20@uom.lk>
2024-02-14 10:30:07 +0530

    Minor changes

--- ts-postgre-server/src/Server.ts
+++ ts-postgre-server/src/Server.ts
@@ -11,7 +11,7 @@ export const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
     cors: {
-        origin: 'http://localhost:3000', // Replace with the origin of your frontend application
+        origin: 'https://ramp-up-99ab1.web.app/', // Replace with the origin of your frontend application
         methods: ['GET', 'POST'],
     },
 });
